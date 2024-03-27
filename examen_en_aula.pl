37cb0f48ab06232c4aecce1d70d8b660cce080a7
MARIEL <lpze.marielalejandra.huayta.qu@unifranz.edu.bo>
2024-03-26 20:19:48 -0400

    examen en aula

---
+++ examen_en_aula.pl
@@ -0,0 +1,28 @@
+esProducto(lg510).
+esProducto(samsungb125).
+esProducto(lgbatimix).
+esProducto(samsungl200).
+esProducto(bose).
+esProducto(lg).
+
+esCategoria(batidora).
+esCategoria(lavadora).
+esCategoria(parlantes).
+esCategoria(electrodomestico).
+esCategoria(entretenimiento).
+
+esCliente(maria).
+
+estaEnCategoria(lg510, batidora).
+estaEnCategoria(samsungb125, batidora).
+estaEnCategoria(lgbatimix, lavadora).
+estaEnCategoria(samsungl200, lavadora).
+estaEnCategoria(bose, parlantes).
+estaEnCategoria(lg, parlantes).
+estaEnCategoria(batidora, electrodomestico).
+estaEnCategoria(lavadora, electrodomestico).
+estaEnCategoria(parlantes, entretenimiento).
+
+esProductoDentroDeCategoria(Producto, Categoria):- esProducto(Producto), esCategoria(Categoria), estaEnCategoria(Producto, Categoria).
+esProductoDentroDeCategoria(Producto, Categoria):- estaEnCategoria(Categoria2,Categoria), esProductoDentroDeCategoria(Producto, Categoria).
+
